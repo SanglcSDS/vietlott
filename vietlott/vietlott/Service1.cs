925c2955cf3b84006ef55440cb384e0ecf72bf87
sanglcsds <sanglc@mitec.com.vn>
2021-12-30 18:13:58 +0700

    create vietlott

---
+++ vietlott/vietlott/Service1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vietlott
+{
+    public partial class Service1 : ServiceBase
+    {
+        const int PORT_NO = 5000;
+        const string SERVER_IP = "127.0.0.1";
+        public Service1()
+        {
+            InitializeComponent();
+        }
+        public void OnDebug()
+        {
+            OnStart(null);
+
+        }
+
+        protected override void OnStart(string[] args)
+        {
+
+            //---listen at the specified IP and port no.---
+            IPAddress localAdd = IPAddress.Parse(SERVER_IP);
+            TcpListener listener = new TcpListener(localAdd, PORT_NO);
+            Console.WriteLine("Listening...");
+            listener.Start();
+
+            //---incoming client connected---
+            TcpClient client = listener.AcceptTcpClient();
+
+            //---get the incoming data through a network stream---
+            NetworkStream nwStream = client.GetStream();
+            byte[] buffer = new byte[client.ReceiveBufferSize];
+
+            //---read incoming stream---
+            int bytesRead = nwStream.Read(buffer, 0, client.ReceiveBufferSize);
+
+            //---convert the data received into a string---
+            string dataReceived = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+            Console.WriteLine("Received : " + dataReceived);
+
+            //---write back the text to the client---
+            Console.WriteLine("Sending back : " + dataReceived);
+            nwStream.Write(buffer, 0, bytesRead);
+            client.Close();
+            listener.Stop();
+            Console.ReadLine();
+
+          //  System.Timers.Timer timer = new System.Timers.Timer();
+         //  
+         //    timer.Interval = 200;
+         //   timer.Elapsed += timer_Elapsed;
+         //   timer.Start();
+        }
+        void timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            string date = e.SignalTime.ToString("yyyyMMdd");
+            Logger.Log(" Timer:" + e.SignalTime);
+
+          
+        }
+
+
+
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
